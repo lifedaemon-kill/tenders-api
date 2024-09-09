1a966232eb08f6ce288c1812858fc3736352eeb7
Ruslan <noname0263@gmail.com>
2024-09-10 02:12:25 +0900

    add schemas

---
+++ schema/user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employee
+(
+    id         UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username   VARCHAR(50) UNIQUE NOT NULL,
+    first_name VARCHAR(50),
+    last_name  VARCHAR(50),
+    created_at TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP        DEFAULT CURRENT_TIMESTAMP
+);
